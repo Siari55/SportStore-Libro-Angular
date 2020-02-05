ffd0301c246a2e5d7db5b966db1e29b0023c2702
Jessica <jessi_dragon55@hotmail.com>
2020-02-05 12:05:45 -0600

    OWO

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { Product } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
+import {Cart } from "../model/cart.model";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "store",
@@ -12,7 +14,7 @@ export class StoreComponent{
     public productsPerPage = 4;
     public selectedPage = 1;
 
-    constructor(private repository: ProductRepository){}
+    constructor(private repository: ProductRepository, private cart:Cart, private router: Router){}
 
     get products(): Product[]{
         let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
@@ -44,5 +46,11 @@ export class StoreComponent{
     /* Implementando la directiva de "counter.directive.tx" */
     get pageCount(): number{
         return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage);
+    }
+
+    /* Se agrega el carrito */
+    addProductToCart(product: Product){
+        this.cart.addLine(product);
+        this.router.navigateByUrl("/cart");
     }
 }
